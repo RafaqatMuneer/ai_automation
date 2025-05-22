aebb8dc2ed5d2f85e6ecdf72556cb86e31548edf
Rafaqat Muneer <rafaqatmunir@gmail.com>
2025-05-22 08:22:05 +0500

    Logging functionality imported and tested

--- day3_PDF_invoice_processor/pdf_processor.py
+++ day3_PDF_invoice_processor/pdf_processor.py
@@ -1,1 +1,18 @@
-import pdfplumber
+import re
+import pandas as pd
+import matplotlib as plt
+import logging
+from typing import List, Dict
+import re
+import logger_util
+
+class InvoiceProcessor:
+    def __init__(self):
+        self.logger = logger_util.LoggerUtility().logger
+        self.logger.info("InvoiceProcessor initialized.")
+
+
+    
+if __name__ == "__main__":
+    generator = InvoiceProcessor()
+
